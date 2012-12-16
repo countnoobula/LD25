3169d832680ccae868f75e2be3d9ab74442e24fb
Shaun van Tonder <shaunmichaelvantonder@gmail.com>
2012-12-16 15:18:34 +0200

    Fixed coordinate problems, and several other bug fixes.

--- src/main/HandlerUnits.java
+++ src/main/HandlerUnits.java
@@ -12,7 +12,7 @@ import resources.Resources;
 import main.Unit;
 
 public class HandlerUnits {
-    
+
     protected GuiGame parent;
     protected Unit[][] unitArray;
     protected Unit[][] currentUnitArray;
@@ -23,14 +23,14 @@ public class HandlerUnits {
     protected BufferedImage spriteSword;
     protected BufferedImage spriteArcher;
     protected BufferedImage spriteMage;
-    
+
     public HandlerUnits(GuiGame parent) {
         this.parent = parent;
         currentUnitArray = new Unit[15][20];
         unitArray = new Unit[50][50];
         unitTypeMap = new HashMap<>();
         unitMap = new HashMap<>();
-        
+
         try {
             spriteSword = ImageIO.read(Resources.class.getResourceAsStream("UnitSword.png"));
             spriteArcher = ImageIO.read(Resources.class.getResourceAsStream("UnitArcher.png"));
@@ -38,28 +38,28 @@ public class HandlerUnits {
         } catch(Exception ex) {
             JOptionPane.showMessageDialog(null, "Failed to load resources for units", "Resource failure", JOptionPane.ERROR_MESSAGE);
         }
-        
+
         registerUnitType(new UnitSword(this));
         registerUnitType(new UnitArcher(this));
         registerUnitType(new UnitMage(this));
-        
+
         UnitSword unitSword = new UnitSword(this);
         unitSword.setLocation(4, 2);
         unitMap.put(1, unitSword);
-        
+
         UnitArcher unitArcher = new UnitArcher(this);
         unitArcher.setLocation(4, 5);
         unitArcher.playerOwned = false;
         unitMap.put(2, unitArcher);
-        
+
         updateUnitPositions();
     }
-    
+
     public boolean isUnitsSelected() {
         boolean unitsSelected = false;
         Set keys = unitMap.keySet();
         Iterator i = keys.iterator();
-        
+
         int entryKey = 0;
         Unit entryUnit = null;
         while(i.hasNext()) {
@@ -71,14 +71,14 @@ public class HandlerUnits {
         }
         return unitsSelected;
     }
-    
+
     public boolean isOccupied(int x, int y) {
         if(unitArray[x][y] != null) {
             return true;
         }
         return false;
     }
-    
+
     public BufferedImage getSprite(int id) {
         switch(id) {
             case 1:
@@ -90,82 +90,82 @@ public class HandlerUnits {
         }
         return null;
     }
-    
+
     public boolean tileHasUnit(int x, int y) {
         if(currentUnitArray[x][y] != null) {
             return true;
         }
         return false;
     }
-    
+
     public void selectUnit(int x, int y) {
         currentUnitArray[x][y].select();
     }
-    
+
     public Unit getUnit(int x, int y) {
         return unitArray[x][y];
     }
-    
+
     public Unit getUnitType(int id) {
         return unitTypeMap.get(id);
     }
-    
+
     public void registerUnitType(Unit unitToRegister) {
         if(unitTypeMap.get(unitToRegister.getID()) == null) {
             unitTypeMap.put(unitToRegister.getID(), unitToRegister);
         }
     }
-    
+
     public void updateCurrentUnits() {
         updateUnitPositions();
-        for(int i = 0; i < 15; i++) {
-            for(int j = 0; j < 20; j++) {
-                currentUnitArray[i][j] = unitArray[parent.handlerPlayer.xPos + i][parent.handlerPlayer.yPos + j];
+        for(int rows = 0; rows < 15; rows++) {
+            for(int cols = 0; cols < 20; cols++) {
+                currentUnitArray[rows][cols] = unitArray[parent.handlerPlayer.xPos + rows][parent.handlerPlayer.yPos + cols];
             }
         }
         needsUpdate = true;
     }
-    
+
     public void renderCurrentUnits() {
         unitRender = new BufferedImage(640, 480, BufferedImage.TYPE_INT_ARGB);
         Graphics g = unitRender.createGraphics();
-        for(int rows = 0; rows < 20; rows++) {
-            for(int cols = 0; cols < 15; cols++) {
-                if(currentUnitArray[cols][rows] != null && getSprite(currentUnitArray[cols][rows].getID()) != null) {
-                    g.drawImage(getSprite(currentUnitArray[cols][rows].getID()), rows * 32, cols * 32, parent);
-                    if(currentUnitArray[cols][rows].isSelected()) {
+        for(int rows = 0; rows < 15; rows++) {
+            for(int cols = 0; cols < 20; cols++) {
+                if(currentUnitArray[rows][cols] != null && getSprite(currentUnitArray[rows][cols].getID()) != null) {
+                    g.drawImage(getSprite(currentUnitArray[rows][cols].getID()), rows * 32, cols * 32, parent);
+                    if(currentUnitArray[rows][cols].isSelected()) {
                         g.setColor(Color.YELLOW);
                         g.drawRect(rows * 32, cols * 32, 32, 32);
                     }
-                    g.drawImage(getHealthBar(currentUnitArray[cols][rows]), rows * 32, cols * 32, parent);
+                    g.drawImage(getHealthBar(currentUnitArray[rows][cols]), rows * 32, cols * 32, parent);
                 }
             }
         }
         needsUpdate = false;
     }
-    
+
     public boolean needsUpdate() {
         return needsUpdate;
     }
-    
+
     public void render(Graphics g) {
         updateUnitPositions();
         updateCurrentUnits();
         renderCurrentUnits();
         g.drawImage(unitRender, 0, 0, parent.getMainWindow());
     }
-    
+
     public void clearUnitArray() {
         unitArray = null;
         unitArray = new Unit[50][50];
     }
-    
+
     public void updateUnitPositions() {
         Set keys = unitMap.keySet();
         Iterator i = keys.iterator();
-        
+
         clearUnitArray();
-        
+
         int entryKey = 0;
         Unit entryUnit = null;
         while(i.hasNext()) {
@@ -174,11 +174,11 @@ public class HandlerUnits {
             unitArray[entryUnit.getLocation().x][entryUnit.getLocation().y] = entryUnit;
         }
     }
-    
+
     public void deselectAll() {
         Set keys = unitMap.keySet();
         Iterator i = keys.iterator();
-        
+
         int entryKey = 0;
         Unit entryUnit = null;
         while(i.hasNext()) {
@@ -187,12 +187,12 @@ public class HandlerUnits {
             unitArray[entryUnit.getLocation().x][entryUnit.getLocation().y].deselect();
         }
     }
-    
+
     public void moveSelected(int x, int y) {
         if(isUnitsSelected()) {
             Set keys = unitMap.keySet();
             Iterator i = keys.iterator();
-            
+
             int entryKey = 0;
             Unit entryUnit = null;
             while(i.hasNext()) {
@@ -204,12 +204,12 @@ public class HandlerUnits {
             }
         }
     }
-    
+
     public void attackSelected(int x, int y) {
         if(isUnitsSelected()) {
             Set keys = unitMap.keySet();
             Iterator i = keys.iterator();
-            
+
             int entryKey = 0;
             Unit entryUnit = null;
             while(i.hasNext()) {
@@ -221,26 +221,26 @@ public class HandlerUnits {
             }
         }
     }
-    
+
     public BufferedImage getHealthBar(Unit unitToMeasure) {
         BufferedImage healthBar = new BufferedImage(32, 4, BufferedImage.TYPE_INT_RGB);
         Graphics g = healthBar.createGraphics();
         
-        int greenWidth = 0;
         /* 
          * Changed the calculations for the healthbars.
          * It just does (32*curHealth) / maxHealth, so it gives the width of the healthbar to use.
          * You can test if it works by yourself by modifying the "currentHealth" var in UnitSword.java
          *      Or just do in a line below : unitToMeasure.currentHealth = 40 (for example) (and don't forget to remove it :p)
          */
-        greenWidth = (int) (( (32 * unitToMeasure.getCurrentHealth()) / unitToMeasure.getMaxHealth()));
-        
+        //greenWidth = (int) (((32 * unitToMeasure.getCurrentHealth()) / unitToMeasure.getMaxHealth()));
+        int greenWidth = (int)(((32 * unitToMeasure.getCurrentHealth()) / unitToMeasure.getMaxHealth()));
+
         g.setColor(Color.GREEN);
         g.fillRect(0, 0, greenWidth, 4);
-        
+
         return healthBar;
     }
-    
+
     public GuiGame getGuiGame() {
         return parent;
     }
